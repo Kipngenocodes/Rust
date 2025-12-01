339be4ae9120d24073e9539be60d6ffe6adfedb8
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-12-01 09:12:41 -0800

    Add first Rust program: hello world

---
+++ learn rust/hello.rs
@@ -0,0 +1,6 @@
+// A simple Rust program that prints "Hello, world!" to the console
+fn main(){
+    println!("Hello, world!");
+    println!("This is my first Rust program.");
+    println!("I am exited to learn Rust! which is a systems programming language for memory safety and performance.");
+}
