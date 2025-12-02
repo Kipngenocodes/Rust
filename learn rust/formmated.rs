7a39e80ad38f204582d4a907234a288f460330e6
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-12-01 16:29:34 -0800

    update

--- learn rust/formmated.rs
+++ learn rust/formmated.rs
@@ -15,4 +15,22 @@ fn main(){
     println!("Base 8:   {:o}", 69420);
     println!("Base 16:  {:x}", 69420);
     println!("Base 16:  {:X}", 69420);
-}
+
+    // Text with a specified width
+    println!("{number:>5}", number=1); // right-aligns number in a space of 5 characters
+    println!("{number:0>5}", number=1); // pads with zeros instead of spaces
+    println!("{number:0<5}", number=1); // left-aligns number in a space of 5 characters
+    println!("{number:^5}", number=1); // centers number in a space of 5 characters
+    // Rust even checks to make sure the correct number of arguments are used.
+    // The next line would cause a compile-time error due to missing arguments
+    println!("My name is {0}, {1} {0}", "James", "Bond");
+
+    let  number: f64 = 1.0;
+    let width: usize = 6;
+    println!("{number:>width$}", number=number, width=width);
+
+//    named parameters
+    format!("{argument}", argument = "test");   // => "test"
+    format!("{name} {}", 1, name = 2);          // => "2 1"
+    format!("{a} {c} {b}", a="a", b='b', c=3);  // => "a 3 b"
+}   
