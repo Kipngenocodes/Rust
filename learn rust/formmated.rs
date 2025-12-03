48eb966ba6d216961b55740bf2f3e659b6018acc
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-12-02 17:28:01 -0800

    update

--- learn rust/formmated.rs
+++ learn rust/formmated.rs
@@ -32,5 +32,5 @@ fn main(){
 //    named parameters
     format!("{argument}", argument = "test");   // => "test"
     format!("{name} {}", 1, name = 2);          // => "2 1"
-    format!("{a} {c} {b}", a="a", b='b', c=3);  // => "a 3 b"
+    format!("{a} {c} {b}", a="a", b='b', c=3);  // => "a 3 b" 
 }   
